49c208d7843d0a970bdd1a4fb11e5c49871b556c
pakoito <pakoitogod@gmail.com>
2013-06-14 07:23:45 +0200

    added activePlayer to StateMachine

--- WartricksProject/wartricks/src/com/wartricks/logic/StateMachine.java
+++ WartricksProject/wartricks/src/com/wartricks/logic/StateMachine.java
@@ -3,6 +3,8 @@ package com.wartricks.logic;
 
 import java.util.Observable;
 
+import com.wartricks.utils.Constants.Players;
+
 public class StateMachine extends Observable {
     enum GameState {
         CHOOSING_CHARACTER, CHOOSING_SKILL, CHOOSING_TARGET, CHOOSING_CONFIRM, RESOLVING_ACTIONS
@@ -10,9 +12,12 @@ public class StateMachine extends Observable {
 
     private GameState currentState;
 
+    private Players activePlayer;
+
     public StateMachine() {
         super();
         currentState = GameState.CHOOSING_CHARACTER;
+        activePlayer = Players.ONE;
     }
 
     public GameState getCurrentState() {
@@ -21,6 +26,14 @@ public class StateMachine extends Observable {
 
     public void setCurrentState(GameState state) {
         currentState = state;
+        this.setChanged();
+        this.notifyObservers(currentState);
+    }
+
+    public void setActivePlayer(Players player) {
+        // To maintain the finite automata consistent we reset it
+        currentState = GameState.CHOOSING_CHARACTER;
+        activePlayer = player;
         this.setChanged();
         this.notifyObservers(currentState);
     }
