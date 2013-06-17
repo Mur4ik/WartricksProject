5de1c2130e0d8b3aade3bb845a45a2d3bf70df69
pakoito <pakoitogod@gmail.com>
2013-06-17 23:16:58 +0200

    removed unused lists

--- WartricksProject/wartricks/src/com/wartricks/components/ActionSequence.java
+++ WartricksProject/wartricks/src/com/wartricks/components/ActionSequence.java
@@ -9,16 +9,10 @@ import com.badlogic.gdx.graphics.Color;
 public class ActionSequence extends Component {
     public LinkedList<Action> onCastActions;
 
-    public LinkedList<Action> onBeginTurnActions;
-
-    public LinkedList<Action> onEndTurnActions;
-
     public Color pathColor;
 
     public ActionSequence(Action move, Color color) {
         onCastActions = new LinkedList<Action>();
-        onBeginTurnActions = new LinkedList<Action>();
-        onEndTurnActions = new LinkedList<Action>();
         onCastActions.addLast(move);
         pathColor = color;
     }
@@ -29,8 +23,6 @@ public class ActionSequence extends Component {
 
     public ActionSequence(Color color) {
         onCastActions = new LinkedList<Action>();
-        onBeginTurnActions = new LinkedList<Action>();
-        onEndTurnActions = new LinkedList<Action>();
         pathColor = color;
     }
 
